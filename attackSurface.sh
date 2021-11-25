e3ef284bf3369a665b4f267016e5c4402380a95c
Pyramid Ronin <pyramidhead@users.noreply.github.com>
2021-11-25 14:53:27 -0800

    Update attackSurface.sh
    added new design features, little bit of planning, little bit of debug output

--- attackSurface.sh
+++ attackSurface.sh
@@ -2,19 +2,33 @@
 # given a domain, define an attack surface of externally accessible hosts
 
 # accept domain variable
+domain=$1
 
 # extract from nslookup query
 nsl=$(nslookup -type=any netflix.com)
 nsl4=$(nsl | egrep -o '(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])' | awk -v RS="[ \n]+" '!n[$0]++')
-# nsl6=$ | still working on this
+# nsl6=$(nsl | egrep -o 'regex and awk go here') still working on this
+# roll the output of both back into finalized nsl
+nsl="$nsl4$nsl6"
+echo 'nsl output = '
+echo $nsl
 
 # extract from host query
 hlu=$(host netflix.com) | egrep -o '(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])' | awk -v RS="[ \n]+" '!n[$0]++')
+echo 'hlu output = '
+echo $hlu
 
 # extract IPs from dig query
 dlu=$(dig netflix.com any) | egrep -o '(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])' | awk -v RS="[ \n]+" '!n[$0]++'
-# extract hostnames from dig query, resolve with nslookup, and add to surface variable
+# extract hostnames from dig query, resolve with nslookup
+# dho=$(dig netflix.com any | egrep -o '(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])' | awk to parse for hostnames goes here | resolving hostnames to ips goes here) still working on this
+# append output of dho to dlu
+echo 'dlu output = '
+echo $dlu
 
-# combine master variable and dedup
+# combine into master variable
 surface="${nsl} ${hlu} ${dlu}"
+# deduplicate rows as we're using a few discovery methods
+
+echo 'final output = '
 echo $surface
