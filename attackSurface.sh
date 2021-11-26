17924089c4c004fb6918f61b6051bd1acd73c5c6
Pyramid Ronin <pyramidhead@users.noreply.github.com>
2021-11-26 13:50:37 -0800

    Update attackSurface.sh
    making sure it's a regex issue

--- attackSurface.sh
+++ attackSurface.sh
@@ -5,7 +5,8 @@
 domain=$1;
 
 # extract from nslookup query
-nsl=$(nslookup -type=any $domain | egrep -o '(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])';
+nsl=$(nslookup -type=any $domain 
+# | egrep -o '(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])';
 # nsl4=$(nsl | egrep -o '(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])' | awk -v RS="[ \n]+" '!n[$0]++');
 # nsl6=$(nsl | egrep -o 'regex and awk go here') still working on this
 # roll the output of both back into finalized nsl
