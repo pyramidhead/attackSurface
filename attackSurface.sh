49a6088d439bed79960f30962345e8d9959c7d7f
Pyramid Ronin <pyramidhead@users.noreply.github.com>
2022-02-25 13:25:53 -0800

    Update attackSurface.sh
    blargh still not happy with this but stopping for now

--- attackSurface.sh
+++ attackSurface.sh
@@ -17,7 +17,7 @@ nsls=$(nslookup -type=any $domain | grep Server | grep -o '[0-9]\{1,3\}\.[0-9]\{
 # extract address subnet
 nsla=$(nslookup -type=any $domain | grep Address | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}')
 # extract nameservers from nslookup query and resolve
-nsldr=$(nslookup -type=any $domain | grep 'nameserver')
+nsldr=$(nslookup -type=any $domain | grep 'nameserver' | grep -o '\:\/\/([^\/?#]+)(?:[\/?#]|$)/i')
 nsld=$(nslookup -type=any $nsldr) | grep '^Address*'* | grep -v "127.0" | cut -c 10-
 nsl=echo "$($nsls $nsla $nslh $nsld)"
 echo 'nsl output ='
