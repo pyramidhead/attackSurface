7139a3532ec000c50a544239b02d1d02676b07ce
Pyramid Ronin <pyramidhead@users.noreply.github.com>
2022-02-28 03:53:43 -0800

    Update attackSurface.sh
    commenting down and working on nslookup

--- attackSurface.sh
+++ attackSurface.sh
@@ -16,44 +16,46 @@ echo $whodat2
 nslookup -type=any $domain | grep Server | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' > ~/git/attackSurface/surface.txt
 # extract address subnet
 nslookup -type=any $domain | grep Address | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' >> ~/git/attackSurface/surface.txt
+echo 'surface as of nslookup='
+cat ~/git/attackSurface.surface.txt
 # extract nameservers from nslookup query and resolve
-nslookup -type=any $domain | grep 'nameserver' >> ~/git/attackSurface/rawName.txt
-cat rawName.txt | sed -r 's/\.$//' | awk '{print $4}' rawName.txt > ~/git/attackSurface/stripName.txt
-sed 's/.$//' ~/git/attackSurface/stripName.txt > ~/git/attackSurface/strippedName.txt
+# nslookup -type=any $domain | grep 'nameserver' >> ~/git/attackSurface/rawName.txt
+# cat rawName.txt | sed -r 's/\.$//' | awk '{print $4}' rawName.txt > ~/git/attackSurface/stripName.txt
+# sed 's/.$//' ~/git/attackSurface/stripName.txt > ~/git/attackSurface/strippedName.txt
 # have nameservers stripped down to fqdn, resolve them next
-nsld=$(nslookup -type=any $nsldr) | grep '^Address*'* | grep -v "127.0" | cut -c 10-
-nsl=echo "$($nsls $nsla $nslh $nsld)"
-echo 'nsl output ='
-echo $nsl
+# nsld=$(nslookup -type=any $nsldr) | grep '^Address*'* | grep -v "127.0" | cut -c 10-
+# nsl=echo "$($nsls $nsla $nslh $nsld)"
+# echo 'nsl output ='
+# echo $nsl
 
 # dig around for zone transfers
-for i in $ nsldr; do
-  digout=$($digout dig $i $domain axfr)
-done
-echo 'dig output ='
-echo $digout
+# for i in $ nsldr; do
+#  digout=$($digout dig $i $domain axfr)
+# done
+# echo 'dig output ='
+# echo $digout
 
 # extract from host query
-hluv4=$(host $domain) | grep "has address" | cut -c 25-
-hluv6=$(host $domain) | grep "has IPv6" | cut -c 30-
-hlumr=$(host $domain) | grep "mail" | sed 's|.* ||' | sed -r 's/\.$//'
-echo 'hluv4 output ='
-echo $hluv4
-echo 'hluv6 output ='
-echo $hluv6
-echo 'hlumr output ='
-echo $hlumr
-hlu=$($hluv4 $hluv6 $hlumr)
-echo 'hlu output ='
-echo $hlu
+# hluv4=$(host $domain) | grep "has address" | cut -c 25-
+# hluv6=$(host $domain) | grep "has IPv6" | cut -c 30-
+# hlumr=$(host $domain) | grep "mail" | sed 's|.* ||' | sed -r 's/\.$//'
+# echo 'hluv4 output ='
+# echo $hluv4
+# echo 'hluv6 output ='
+# echo $hluv6
+# echo 'hlumr output ='
+# echo $hlumr
+# hlu=$($hluv4 $hluv6 $hlumr)
+# echo 'hlu output ='
+# echo $hlu
 
 # combine into master variables
 humint-"${whodat1} ${whodat2}"
-ipsurface="${nsl} ${hlu}";
+# ipsurface="${nsl} ${hlu}";
 # deduplicate rows as we're using multiple discovery methods
 
 echo 'human intelligence =';
 echo $humint
 
-echo 'ip surface =';
-echo $ipsurface;
+# echo 'ip surface =';
+# echo $ipsurface;
