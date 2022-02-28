c1a157043b124c86ed866b81da14090d172f3216
Pyramid Ronin <pyramidhead@users.noreply.github.com>
2022-02-28 03:55:10 -0800

    Update attackSurface.sh
    typo

--- attackSurface.sh
+++ attackSurface.sh
@@ -17,7 +17,7 @@ nslookup -type=any $domain | grep Server | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.
 # extract address subnet
 nslookup -type=any $domain | grep Address | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' >> ~/git/attackSurface/surface.txt
 echo 'surface as of nslookup='
-cat ~/git/attackSurface.surface.txt
+cat ~/git/attackSurface/surface.txt
 # extract nameservers from nslookup query and resolve
 # nslookup -type=any $domain | grep 'nameserver' >> ~/git/attackSurface/rawName.txt
 # cat rawName.txt | sed -r 's/\.$//' | awk '{print $4}' rawName.txt > ~/git/attackSurface/stripName.txt
