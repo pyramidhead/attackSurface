b7da036b638d49287ab6b924842f1b7e770186bd
Pyramid Ronin <pyramidhead@users.noreply.github.com>
2022-03-14 18:49:03 -0700

    Update attackSurface.sh
    adding first dig method for testing

--- attackSurface.sh
+++ attackSurface.sh
@@ -39,6 +39,14 @@ cat ~/git/attackSurface/surface.txt
 dig $domain ANY +nostat +nocmd +nocomments > ~/git/attackSurface/digOut.txt
 echo 'dig output='
 cat ~/git/attackSurface/digOut.txt
+while read -r line; do
+# this is incomplete
+# extract naked IPs
+  grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' digOut.txt >> /home/ec2-user/git/attackSurface/surface.txt
+# resolve hostnames and extract (remember to throw away $domain)
+; done;
+echo 'dig output added='
+cat /home/ec2-user/git/attackSurface/surface.txt
 # for h in $( cat ~/git/attackSurface/strippedName.txt ); do
 #    a=$(dig +short $h | head -n1)
 #    echo -e "$h\t${a:-Did_Not_Resolve}"
