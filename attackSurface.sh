83bdc19ee1c1baf3d53d018ebbdd8d655d05829a
Pyramid Ronin <pyramidhead@users.noreply.github.com>
2022-05-02 04:12:02 -0700

    Update attackSurface.sh
    after testing, converting from text to variable

--- attackSurface.sh
+++ attackSurface.sh
@@ -17,8 +17,9 @@ nameserver 1.1.1.1' | sudo dd of=/etc/resolv.conf
 whois -H whois.arin.net "o $domain" > ~/git/attackSurface/humint.txt
 whois $domain >> ~/git/attackSurface/humint.txt
 
-# parse humint.txt for IPs and hostnames
-grep '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' humint.txt > ~/git/attackSurface/surface.txt
+# parse humint.txt for IPs
+surface=$(grep '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' humint.txt)
+echo $surface
 
 # parse humint.txt for hostnames and lookup IPs
 grep 'Server' humint.txt > ~/git/attackSurface/humHostsRaw.txt
